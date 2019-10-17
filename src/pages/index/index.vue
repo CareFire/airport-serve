29fa3c050d61622a9c812d83fe0e7093150eb04a
zhaotl <768021130@qq.com>
2019-10-17 10:45:41 +0800

    update

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="wrapper">
     <image class="head-image gray" src="/static/images/vipcard.png"/>
-    <van-button type="primary">主要按钮</van-button>
+    <div id="notice-div">
+      <div class="notice-middle-div">
+        <p>剩余次数</p>
+        <p>0</p>
+      </div>
+      <div class="notice-middle-div">
+        <p>已用次数</p>
+        <p>0</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -30,6 +39,8 @@ export default {
 }
 .head-image {
   width:100%;
+  height: 400rpx;
+  border-radius:3%;
 }
 .gray {
   -webkit-filter: grayscale(100%);
@@ -38,5 +49,19 @@ export default {
   -o-filter: grayscale(100%);
   filter: grayscale(100%);
   filter: gray;
+}
+
+#notice-div {
+  width: 100%;
+}
+
+.notice-middle-div {
+  width:48%;
+  margin:0 1%;
+  float: left;
+  text-align: center;
+  box-sizing: border-box;
+  border: 1rpx solid #ddd;
+  border-radius: 5%;
 }
 </style>
